1913e9e1a750ff7d264d4d53f4f696be407d0c5f
Dvoey <lluusocial123@gmail.com>
2016-05-18 16:01:19 +0200

    Test

--- GrafikTest.py
+++ GrafikTest.py
@@ -7,30 +7,28 @@ def main():
     head = Circle(Point(40,100), 52) # set center and radius
     head.setFill("yellow")
     head.draw(win)
-    #
+
     eye1 = Circle(Point(30, 105), 5)
     eye1.setFill('blue')
     eye1.draw(win)
-    #
-    win.getMouse()
-    eye2 = Line(Point(45, 105), 7) # set endpoints
-    eye2.setFill('black')
+
+    #win.getMouse()
+    eye2 = Circle(Point(45, 105), Point(55, 105)) # set endpoints
+    eye2.setFill('blue')
     eye2.setWidth(10)
     eye2.draw(win)
 
-    #
     win.getMouse()
     mouth = Oval(Point(30, 120), Point(50, 85)) # set corners of bounding box
     mouth.setFill("red")
     mouth.draw(win)
-    #
+
     win.getMouse()
     label = Text(Point(100, 120), 'A face')
     label.setFill('red')
     label.draw(win)
 
     win.getMouse()
-
     win.close()
-    #
+
 main()
