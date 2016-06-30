bcd2bc2fe338a70333e8d5572ff17492f97fb4a6
Dvoey <lluusocial123@gmail.com>
2016-06-30 15:23:46 +0200

    Test

--- Tamazubi.py
+++ Tamazubi.py
@@ -2,48 +2,51 @@
 
 class Tamazubi(object):
 
-    def __init__(self,name,alter,ausbildungstyp):
-        self.name = name
-        self.alter = alter
-        self.ausbildungstyp = ausbildungstyp
+    def __init__(self,pfad):
+        self.name = ""
+        self.alter = -1
+        self.ausbildungstyp = ""
+
+        self.laden(pfad)
+
+        if self.name == "":
+            self.name = raw_input('Wie heißt du?')
+
+        if self.alter == -1:
+            self.alter = int(raw_input('Wie alt bist du?'))
+
+        if self.ausbildungstyp == "":
+            self.ausbildungstyp = raw_input('Was ist deine Ausbildung?')
+
 
     def speichern(self):
         text = open('c:/temp/test.txt', 'w')
         text.write("NAME="+self.name+"\n")
-        text.write("ALTER="+self.alter+"\n")
+        text.write("ALTER="+str(self.alter)+"\n")
         text.write("AUSBILDUNG="+self.ausbildungstyp+"\n")
         text.close()
 
-    def laden(self):
-        text = open('c:/temp/test.txt', 'r')
+    def laden(self,pfad):
+        text = open(pfad, 'r')
 
-        # do
-        #    zeile = readline()
-        #    if zeile == ""
-        #        break;
-        #    split...
-        #    if NAME
-        #       ...
-        #    ..
-
-
-        for i in range(3):
+        while True:
             zeile = text.readline()
+            if zeile == '':
+                break
+
             split = zeile.split("=", 1)
 
             if split[0] == 'NAME':
                 self.name = split[1]
 
             if split[0] == 'ALTER':
-                self.alter = split[1]
+                self.alter = int(split[1])
 
             if split[0] == 'AUSBILDUNG':
                 self.ausbildungstyp = split[1]
 
-azubiname = raw_input('Wie heißt du?')
-azubialter = raw_input('Wie alt bist du?')
-azubityp = raw_input('Was ist deine Ausbildung?')
+        text.close()
 
-t = Tamazubi(azubiname,azubialter,azubityp)
+t = Tamazubi('c:/temp/test.txt') #azubiname,azubialter,azubityp)
 t.speichern()
-t.laden()
+#t.laden()
