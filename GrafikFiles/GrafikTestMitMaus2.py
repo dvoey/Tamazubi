3c34c2babdb8e178958fba079aa49744c66a7a78
Dvoey <lluusocial123@gmail.com>
2016-09-02 11:23:31 +0200

    Test

--- GrafikFiles/GrafikTestMitMaus2.py
+++ GrafikFiles/GrafikTestMitMaus2.py
@@ -5,40 +5,16 @@ import random
 def main():
     win = GraphWin('FaceTest', 500, 500) # give title and dimensions
 
-    # entry = Entry(Point(300,150),50)
-    # entry.setFill("white")
-    # entry.draw(win)
-    #
-    # text = entry.getText()
+    entry = Entry(Point(300,150),50)
+    entry.setFill("white")
+    entry.draw(win)
 
-    head = Circle(Point(250, 100), 52) # set center and radius
+    text = entry.getText()
+
+    head = Circle(Point(40,100), 52) # set center and radius
     head.setFill("yellow")
     head.draw(win)
-
-    koerper = Oval(Point(200, 150), Point(300,300)) # set center and radius
-    koerper.setFill("black")
-    koerper.draw(win)
-
-    rArm = Line(Point(220, 150), Point(140, 250))  # rArm
-    rArm.setWidth(15)
-    rArm.setFill("red")
-    rArm.draw(win)
-
-    lArm = Line(Point(280, 150), Point(340, 250))  # lArm
-    lArm.setWidth(15)
-    lArm.setFill("blue")
-    lArm.draw(win)
-
-    rLeg = Line(Point(280, 285), Point(340, 450))  # rLeg
-    rLeg.setWidth(15)
-    rLeg.setFill("green")
-    rLeg.draw(win)
-
-    lLeg = Line(Point(220, 285), Point(140, 450))  # lLeg
-    lLeg.setWidth(15)
-    lLeg.setFill("purple")
-    lLeg.draw(win)
-
+    #
     win.mainloop()
     # for i in range(10):
     #     eye1 = Circle(Point(random.randint(0,500), random.randint(0,500)), 5)
--- TamazubiMain.py
+++ TamazubiMain.py
@@ -1,27 +1,43 @@
-from graphics import *
 from Tamazubi import *
+from graphics import *
 
 
 class TamazubiMain(object):
 
+    @staticmethod
     def main():
-        tamazubi = Tamazubi()
+        #tamazubi = Tamazubi()
 
         win = GraphWin('Tamazubi', 500, 500) # give title and dimensions
 
-        paintTamazubi(win)
-        #
+        t.paintTamazubi(win)
+
         win.mainloop()
 
         win.close()
 
 
     def paintTamazubi(window):
-        head = Circle(Point(40,100), 52) # set center and radius
+        head = Circle(Point(40,100), 52) # set center and radius        #head
         head.setFill("yellow")
         head.draw(window)
 
+        rArm = Line(Point(40, 100), 52)   #rArm
+        rArm.setFill("red")
+        rArm.draw(window)
 
-main()
+        lArm = Line(Point(40, 100), 52)     #lArm
+        lArm.setFill("blue")
+        lArm.draw(window)
+
+        rLeg = Line(Point(40, 100), 52)       #rLeg
+        rLeg.setFill("green")
+        rLeg.draw(window)
+
+        lLeg = Line(Point(40, 100), 52)       #lLeg
+        lLeg.setFill("purple")
+        lLeg.draw(window)
+
+TamazubiMain.main()
 
 
